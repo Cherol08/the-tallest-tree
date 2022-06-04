c7b064a1adb2c58f29567f24a7cde78a0c6233ec
Cherol Phoshoko <leelo.cherol@gmail.com>
2022-06-04 20:29:36 +0200

    Updated endpoints

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express();
 const userController = require('../controllers/userController')
 
-router.get('/', userController.getRoot)
+router.get('/', userController.getUsers)
 router.post('/createUser', userController.createUser)
 
 module.exports = router; 
